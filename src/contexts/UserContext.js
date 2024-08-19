1b072c30c567b16069ed2189c1b7e830ebc7e0cd
Anis <dev.private1002@gmail.com>
2024-08-18 23:42:55 -0500

    Updated StartWeight Logic

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,12 +1,13 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
+  const [uid, setUid] = useState("8Qw8d1u3HNgz6yXXTw694FD8Vc62");
 
   return (
-    <UserContext.Provider value={{ username, setUsername }}>
+    <UserContext.Provider value={{ username, setUsername, uid, setUid }}>
       {children}
     </UserContext.Provider>
   );
--- src/pages/StartWeightScreen/StartWeightScreen.jsx
+++ src/pages/StartWeightScreen/StartWeightScreen.jsx
@@ -1,27 +1,70 @@
-import React from "react";
-import WeightInputScreen from "../../component/WeightInputScreen/WeightInputScreen";
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import WeightInputScreen from "../../component/WeightInputScreen/WeightInputScreen";
+import {
+  checkHealthConnection,
+  getUserWeights,
+  mutationUserWeights,
+} from "../../firebaseApis/healthApis";
+import { GlobalContext } from "../../contexts/GlobalContext";
+import { UserContext } from "../../contexts/UserContext";
+import Loading from "../Loading/Loading";
 
 function StartWeightScreen() {
   const navigate = useNavigate();
-  const handleNext = () => {
-    navigate("/current-weight");
+  const { userWeights, setUserWeights } = useContext(GlobalContext);
+  const [weight, setWeight] = useState(102);
+  const [loading, setLoading] = useState(true);
+  const { uid } = useContext(UserContext);
+
+  useEffect(() => {
+    checkHealthConnection({ uid })
+      .then((res) => {
+        if (res.data.result) {
+          getUserWeights({ uid })
+            .then((res) => {
+              setUserWeights(res.data.data);
+              setLoading(false);
+            })
+            .catch((err) => {
+              console.error(err);
+              setLoading(false);
+            });
+        } else {
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+
+  const currentWeightPath = "/current-weight";
+
+  const handleNextAndSkip = () => {
+    mutationUserWeights({ uid, key: "start_weight", value: weight })
+      .then((res) => {
+        if (res.data.result) navigate(currentWeightPath);
+      })
+      .catch((err) => console.error(err));
   };
-  const handleSkip = () => {
-    navigate("/current-weight");
-  };
-  const handleBack = () => {
-    navigate(-1);
-  };
+  const handleBack = () => navigate(-1);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <WeightInputScreen
       title="I need to know your weight to help you track the progress!"
       subtitle="Please enter your start weight"
-      placeholder="Today Weight"
+      placeholder="Start Weight"
       buttonText="Done!"
-      initialValue="102"
-      onNext={handleNext}
-      onSkip={handleSkip}
+      initialValue={!userWeights ? weight : userWeights.start_weight}
+      onChange={(e) => setWeight(e)}
+      onNext={handleNextAndSkip}
+      onSkip={handleNextAndSkip}
       onBack={handleBack}
     />
   );
